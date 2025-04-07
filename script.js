e4e1079ace5b005645b3ab589fc8c7fb282a9543
AnatoliiChumakde <Tolik4umakde@gmail.com>
2025-04-08 00:57:12 +0200

    commit3
    commit3

--- script.js
+++ script.js
@@ -23,15 +23,16 @@ const xmenu = document.querySelector('.xmenu')
 
 const people = [
     'https://drscdn.500px.org/photo/1111556548/q%3D90_m%3D2048/v2?sig=e2f075d4596320b4e8a04a2236a94683643f3bc58cc65a4de5454d9d8785f6f0',
-    'https://lh3.google.com/u/0/d/1868wnbyic3e3hWMee0lTL7K5u4FKnuR4=w1280-h1318-iv1?auditContext=forDisplay',
-    'https://lh3.google.com/u/0/d/1RmAXrb8wENgt8Vbt334aZ39zGcfLl_N3=w636-h1319-iv1',
-    'https://lh3.google.com/u/0/d/1i6ksiyR-m181Y2VbgmcGEgEsP6c86pwO=w636-h1319-iv1',
-    'https://lh3.google.com/u/0/d/1qZY43vDmD9rmZllwV7W0PpLk4WvwpnsX=w636-h1319-iv2',
-    'https://lh3.google.com/u/0/d/1rH6R7TC85v51JmQpLLYJ9plHG6BYSNHN=w635-h1319-iv2',
-    'https://lh3.google.com/u/0/d/1rGnJI7voGF58o_Zx0YrbI2OBSlhbvQZP=w1280-h1319-iv2',
-    'https://lh3.google.com/u/0/d/1rObyJ0k9Ph_lewi_xmwI9GPMq283EsKG=w1280-h1319-iv2',
-    'https://lh3.google.com/u/0/d/1rMrAtQ62grXF0Kzpfrq0a_1uEuThgnu_=w1280-h1319-iv2?auditContext=forDisplay',
-
+    'https://drscdn.500px.org/photo/1111556687/q%3D90_m%3D2048/v2?sig=11094914a71ad2dacf82f60e2aebdd72594dcc2e97e404c72c6ad576a70bd5e7',
+    'https://drscdn.500px.org/photo/1111556689/q%3D90_m%3D2048/v2?sig=baa6e4e7718fbc052deaccd3ceaba9d25fcd2e57e2c2e93ee76c828b81331843',
+    'https://drscdn.500px.org/photo/1111556694/q%3D90_m%3D2048/v2?sig=60a87431d57712e9e43f8953b6fd4c390343370c0808aa58aeb4ee537afeee06',
+    'https://drscdn.500px.org/photo/1111556693/q%3D90_m%3D2048/v2?sig=afeec72a18fab8f902ae1cc217b05360173750e6a307c7464695508ff39fb39e',
+    'https://drscdn.500px.org/photo/1111556688/q%3D90_m%3D2048/v2?sig=37cfb1918687efacfb57e52f10f576e5c67764bdd67773209cac6180e2773272',
+    'https://drscdn.500px.org/photo/1111556690/q%3D90_m%3D2048/v2?sig=2266c8260c2cfb30663e53c5ad0d485b56e08544f45527c9e14513f9aa86567a',
+    'https://drscdn.500px.org/photo/1111556691/q%3D90_m%3D2048/v2?sig=123fbea32c6479dcee71252b900ea619254ca862e7adff5c5c9486f7723cd615',
+    'https://drscdn.500px.org/photo/1111556692/q%3D90_m%3D2048/v2?sig=af7b1517dfb7f46e1d7181054fc2b82705eeaa9f6b9700130db20e730a3cf33d',
+    'https://drscdn.500px.org/photo/1111556696/q%3D90_m%3D2048/v2?sig=ba50ad3086d945e6a77568d713e3e4aeb98a87998785a0ba294e3037bfc7e4e2',
+    'https://drscdn.500px.org/photo/1111556695/q%3D90_m%3D2048/v2?sig=e09a5296e7a7076ff178b8204d8411c110b9bca818e622ebc9c70108c2b8b806',
 ]
 const children = [
     'https://lh3.google.com/u/0/d/1k67TAbk05LOZ1S_WSaOj0b6EiRQ_4m5d=w1279-h1318-iv5',
@@ -54,7 +55,7 @@ window.addEventListener('scroll', function() {
     }
   });
 
-let curArr = [...children]
+let curArr = [...people]
 let curId = 0
 
 
